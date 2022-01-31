2c0966dadc8fb0f526e6e4b76dde1d3baa3518a7
zaion998 <89512034+zaion998@users.noreply.github.com>
2022-02-01 05:07:00 +0800

    FINAL

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
 const button = document.querySelector(".container button");
-const jokeDiv = document.querySelector(".container .joke p");
+const quote = document.querySelector(".container p");
 
-document.addEventListener("DOMContentLoaded", getJock);
 
-button.addEventListener("click", getJock);
+btn.addEventListener('click', getWord);
 
-async function getJock() {
-  const jokeData = await fetch("https://icanhazdadjoke.com/", {
-    headers: {
-      Accept: "application/json"
-    }
-  });
-  const jokeObj = await jokeData.json();
-  jokeDiv.innerHTML = jokeObj.joke;
-  console.log(jokeData);
-}
-// *********************
-// This Code is for only the floating card in right bottom corner
-// **********************
+function getWord(){
+  fetch('https://animechan.vercel.app/api/random')
+    .then(res => res.json())
+    .then(data => { 
+      quote.innerHTML = data.quote;
+      anime.innerHTML = data.anime;
+      character.innerHTML = data.character;
 
-const touchButton = document.querySelector(".float-text");
-const card = document.querySelector(".float-card-info");
-const close = document.querySelector(".gg-close-r");
-
-touchButton.addEventListener("click", moveCard);
-close.addEventListener("click", moveCard);
-
-function moveCard() {
-  card.classList.toggle("active");
+})
 }
